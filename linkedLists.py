162f9215489a878ecb8f3524bf5488d0265dad2b
User01 <user01@localhost.localdomain>
2018-09-22 22:20:27 -0400

    Initial Commit

---
+++ linkedLists.py
@@ -0,0 +1,113 @@
+class SLNode(object):
+
+ def __init__(self, data=None, next_node=None):
+  self.data = data
+  self.next_node = next_node
+
+ def get_data(self):
+  return self.data
+
+ def get_next(self):
+  return self.next_node
+
+ def set_next(self, next_node):
+  self.next_node = next_node
+
+ def make_tail(tail_node=None):
+  self.next_node = tail_node
+
+class DLNode(object):
+
+ def __init__(self, data=None, prev_node=None, next_node=None):
+  self.data = data
+  self.next_node = next_node
+  self.prev_node = prev_node
+
+ def get_data(self):
+  return self.data
+
+ def get_next(self):
+  return self.next_node
+
+ def get_prev(self):
+  return self.prev_node
+
+ def set_next(self, next_node):
+  self.next_node = next_node
+
+ def set_prev(self, prev_node):
+  self.prev_node = prev_node
+
+ def make_tail(tail_node=None):
+  self.next_node = tail_node
+
+ def make_root(root_node=None):
+  self.prev_node = root_node
+
+class SLList(object):
+
+ def __init__(self, root=None):
+  self.root = root
+
+ def insert(self, node):
+  node = SLNode(node)
+  node.set_next(self.root)
+  self.root = node
+
+ def get_last(self):
+  node = self.root
+  
+ def size(self):
+  count, current = 0, self.root
+  while current:
+   count +=1
+   current = current.get_next()
+  return count
+
+ def search(self, item):
+  current =  self.root
+  while current:
+   if current.get_data()==item: return True
+   current = current.get_next()
+  return False
+
+ def delete(self, item):
+  prev, current = None, self.root
+  while current:
+   if current.get_data() == item:
+    if prev: prev.set_next(current.get_next())
+    else: self.root = current.get_next()
+    return True
+   prev, current = current, current.get_next()
+  return False
+
+ def get_all(self):
+  current = self.root
+  while current:
+   yield current.get_data()
+   current = current.get_next()
+
+ def print_all(self):
+  print(list(self.get_all()))
+
+
+if __name__ == '__main__':
+ temp = SLList()
+ temp.insert(1)
+ temp.insert(2)
+ temp.insert(3)
+ print(temp.size())
+ temp.print_all()
+ temp.insert(4)
+ temp.print_all()
+ temp.delete(2)
+ temp.print_all()
+ 
+
+
+   
+
+ 
+   
+  
+   
