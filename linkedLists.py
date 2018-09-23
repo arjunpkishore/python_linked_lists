303ac255b78f2550425e4c206856689a4ff30afe
Arjun Parukkandy Kishore <arjunpk.fb@gmail.com>
2018-09-23 14:29:52 -0400

    Enhancements

--- linkedLists.py
+++ linkedLists.py
@@ -48,19 +48,27 @@ class SLList(object):
 
  def __init__(self, root=None):
   self.root = root
+  self.current = root
   self.tail = root
 
  def __insert_head(self, node):
-  node = SLNode(node)
+  if not isinstance(node, SLNode):
+   node = SLNode(node)
   node.set_next(self.root)
   self.root = node
   if not self.tail: self.tail = self.root
 
  def __insert_tail(self, node):
-  node = SLNode(node)
-  node.set_next(None)
-  self.tail.set_next(node)
-
+  if not isinstance(node, SLNode):
+   node = SLNode(node)
+  if self.tail: 
+   self.tail.set_next(node)
+   while True:
+    next_node  = self.tail.get_next()
+    if not next_node: break
+    self.tail = next_node
+  else: self.__insert_head(node)
+    
  def insert(self, node, type='head'):
   {
    'head' : self.__insert_head,
@@ -69,7 +77,7 @@ class SLList(object):
   
 
  def get_last(self):
-  node = self.root
+  return self.tail
   
  def size(self):
   count, current = 0, self.root
@@ -104,6 +112,34 @@ class SLList(object):
  def print_all(self):
   print(list(self.get_all()))
 
+ def get_data(self):
+  return self.current.get_data()
+
+ def get_next(self):
+  prev = self.current
+  if prev: self.current = self.current.get_next()
+  return prev 
+
+ def get(self):
+  return self.current
+  
+
+ def reset(self):
+  self.current = self.root
+
+def merge_sorted_SLList(L1, L2):
+ temp = SLList()
+ while L1.get() and L2.get():
+  if L1.get_data() > L2.get_data():
+   temp.insert(L1.get_data(), 'tail')
+   L1.get_next()
+  else:
+   temp.insert(L2.get_data(), 'tail')
+   L2.get_next()
+
+ temp.insert(L1.get_next() or L2.get_next(), 'tail')
+ return temp
+ 
 
 if __name__ == '__main__':
  temp = SLList()
@@ -120,7 +156,20 @@ if __name__ == '__main__':
  temp.print_all()
  print (temp.size())
  
+ L1 = SLList()
+ L2 = SLList()
+ 
+ L1.insert(7)
+ L1.insert(5)
+ L1.insert(2)
+ L1.print_all()
 
+ L2.insert(11)
+ L2.insert(3)
+ L2.print_all()
+
+ L = merge_sorted_SLList(L1, L2)
+ L.print_all()
 
    
 
